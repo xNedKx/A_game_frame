79b1bfa5f26bde33548b460859e1182a7d8b525e
xNedKx <xnedkx@gmail.com>
2014-07-16 01:23:14 +0800

    project1

---
+++ project1/lines.js
@@ -0,0 +1,20 @@
+lines=[
+'<div style="text-align:center;"><span style="font-size:3em;" spd=200>一　個　懸　疑　故　事</span></div>',
+'<bg="./BG1.jpg" p="0 -100px" w=800>',
+'<color="#111" /><span style="font-weight:bold;">在劍橋大學的公園裡，張庭瑜被尖銳的匕首畫破喉嚨一刀斃命。</span>',
+'<span style="font-weight:bold;">雪地上除了張庭瑜自己的足跡之外，只留下了另外兩個不像是人類腳印的痕跡。</span>',
+'<span style="font-weight:bold;">究竟是命運的糾纏，還是感情的糾葛，或者是金錢上的糾紛？\n\n到底兇手是誰？\n\n用什麼手法殺人？</span>',
+'<span style="font-weight:bold;">背後是否隱藏著什麼不可告人的祕密呢？\n\n\n\n\n\n讓我們繼續看下去...</span>',
+'<bg=""><color="#333"><img src="./C1.jpg" w=300 x=-80 y=80><span style="color:#F05959;font-weight:bold;" spd=0>嫌疑犯1：葉亞笛</span>\n\n  主治醫師的女友，因為跟張庭瑜劈腿，被庭瑜以此威脅，\n如果不讓他庭瑜下班後使用主治醫師的辦公室吹冷氣，\n就要向主治醫師公開兩人劈腿的內容。因此亞笛對庭瑜產生殺意。',
+'<img src="./C2.jpg" w=300 x=-80 y=80><span style="color:#F05959;font-weight:bold;" spd=0>嫌疑犯2：邱昌庭</span>\n\n  與庭瑜曾是同窗同學，因兩人常在名次上競爭，\n加上女友吳台蘭被庭瑜搶走，所以對庭瑜懷有恨意。\n\n  興趣是計算物理算式。',
+'<img src="./C3.jpg" w=300 x=-80 y=80><span style="color:#F05959;font-weight:bold;" spd=0>嫌疑犯3：吳台蘭</span>\n\n  舞台劇團的第一女主角，從小學習芭蕾舞，是公認的第一舞者，\n曾與庭瑜交往一段時間，卻被無情的甩掉，因此對庭瑜怨恨在心。',
+'<img src="./C4.jpg" w=300 x=-80 y=80><span style="color:#6666E5;font-weight:bold;" spd=0>金甲一：</span>\n\n  有這些資訊就夠了！',
+'<img src="./C4.jpg" w=300 x=-80 y=80><span style="color:#6666E5;font-weight:bold;" spd=0>金甲一：</span>\n\n  張庭瑜死亡現場的照片已經清楚地拍出了兇手犯案的線索！',
+'<img src="./C4.jpg" w=300 x=-80 y=80><span style="color:#6666E5;font-weight:bold;" spd=0>金甲一：</span>\n\n  這個線索就是從嫌疑犯中找出兇手的重要證據！',
+'<img src="./C4.jpg" w=300 x=-80 y=80><span style="color:#6666E5;font-weight:bold;" spd=0>金甲一：</span>\n\n  就讓我以虛構爺爺的名字發誓，一定要找出殺害庭瑜的兇手！！',
+'<img src="./C1.jpg" w=200 h=320 x=50 y=60 style="width:320px;height:320px;left:-30px;position:absolute;" gt=14><img src="./C2.jpg" w=200 h=320 x=300 y=60 style="width:320px;height:320px;left:-30px;position:absolute;" gt=14><img src="./C3.jpg" w=200 h=320 x=550 y=60 style="width:320px;height:320px;left:-30px;position:absolute;" gt=15><span spd=0>究竟是誰殺了張庭瑜呢？</span>\n\n<a gt=14>[葉亞笛]</a>\n<a gt=14>[邱昌庭]</a>\n<a gt=15>[吳台蘭]</a>',
+'<img src="./C4.jpg" w=300 x=-80 y=80><span style="color:#6666E5;font-weight:bold;" spd=0>金甲一：</span>\n\n  你的推理能力不夠喔。\n\n\n\n<div style="text-align:right;"><a gt=0 style="color:#666">[重頭來過]</a></div>',
+'<img src="./C4.jpg" w=300 x=-80 y=80><span style="color:#6666E5;font-weight:bold;" spd=0>金甲一：</span>\n\n  沒錯！兇手就是吳台蘭！',
+'<img src="./C4.jpg" w=300 x=-80 y=80><span style="color:#6666E5;font-weight:bold;" spd=0>金甲一：</span>\n\n  吳台蘭先假裝腳部受傷，誘使庭瑜背著她走入公園後，\n拿出藏在身上的匕首從後面劃破庭瑜的喉嚨，\n接著再利用從小學習的芭蕾舞技巧，\n使用腳趾尖端小心翼翼地從雪地上不留足跡地離開公園。',
+'<div style="text-align:center;"><span style="font-size:3em;" spd=200>成　功　破　案　！</span></div>\n\n\n<div style="text-align:right;"><a style="color:#666;" gt=0>[再玩一次]</a></div>'
+]
