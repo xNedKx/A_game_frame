1960815f4b66bcf78db7e3b1b9f7b713923732f4
xNedKx <xnedkx@gmail.com>
2014-07-17 00:51:56 +0800

    vars if condition

---
+++ lines.js
@@ -0,0 +1,17 @@
+lines=[
+'<spd=1/>歡迎使用對話框測試小程式，請<span style="font-weight:bold;color:#F00" spd=300>按一下</span>繼續閱讀。',
+'目前右下角提示指標速度是0.5秒/0.5秒(顯示/隱藏)',
+'預設文字顯示速度是60ms，一次一個字元。\n可使用<\u0000spd=0>或<\u0000span spd=60>變速文字<\u0000/span>設定速度，每次對話都會恢復。顯示中點一下會加速到全顯。',
+'對話是回不去的喔～　^.<\n\n(GOOGLE圖片會到..?)<a gt>\n</a><a gt>NEXT</a><img src="https://www.google.com.tw/images/srpr/logo11w.png" h=100 x=100 y=100 gt=0><img src="https://www.google.com.tw/images/srpr/logo11w.png" w=100 x=500 y=300 gt=-0>',
+'對話到最後一句，你按再多次也不會有反應。',
+'圖片測試...\n\n\n\n\n<bg="https://www.google.com.tw/images/srpr/logo11w.png" p="center">\n\n\nGOOGLE!',
+'<bg=""/>換\n行\n測\n試\n......使用\\n會自動轉換為<br>tag。',
+'     空 白  測   試     \n\n半形空白會自動轉變成&nbsp;，所以不會自動縮排。',
+'連結測試...\n\n\n<a gt=0>選項一 回到開頭</a>\n<a gt=-0>選項二 到結束</a>\n\n<a gt>下一頁...</a>',
+'<spd=1/>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+'速度1ms的狂飆',
+'<spd=20/>The quick brown fox jumps over the lazy dog.',
+'<spd=10/>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n\nBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\n\nCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n\nDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD\n\nEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE',
+'排版還是會依瀏覽器不同改變.. 大概還需要調整。',
+'<span style="color:#000;" spd=0>  >>>>>  >   > >>>>>       >>>>> >   > >>>>  \n  >>>>>  >   > >>>>>       >>>>> >>  > >>>>> \n    >    >   > >           >     >>  > >   > \n    >    >>>>> >>>>>       >>>>> >>> > >   > \n    >    >>>>> >>>>>       >>>>> > >>> >   > \n    >    >   > >           >     >  >> >   > \n    >    >   > >>>>>       >>>>> >  >> >>>>> \n    >    >   > >>>>>       >>>>> >   > >>>>  \n                                             </span>'
+];
