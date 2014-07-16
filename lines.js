7205362a1945e3e8c7a23c4e35fa5dd18314e8b2
xNedKx <xnedkx@gmail.com>
2014-07-17 01:23:48 +0800

    little modify

--- lines.js
+++ lines.js
@@ -1,6 +1,6 @@
 lines=[
 '<div style="text-align:center;font-size:3em;font-weight:nold;">測　試　展　示　程　式</div><var n="loop" v="-1" />',
 '<div>迴圈測試</div><p>LOOP: <var n="loop" v="+1" s /></p><p><if n="loop" c="ME" v="5">繞5次囉～<if n="loop" c="ME" v="10"> 別再繞啦～</if></if></p>',
-'<p><a gt=1>[進入迴圈]</a></p><p> </p><p><a gt>離開迴圈</a></p>',
+'<p><a gt=1>[進入迴圈]</a></p><p> </p><p><a gt>[離開迴圈]</a></p>',
 '尚未新增其他測試。'
 ];
