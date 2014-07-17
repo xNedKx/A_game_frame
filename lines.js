661569a62a9c957aa124f7c321da42137031d142
xNedKx <xnedkx@gmail.com>
2014-07-17 13:10:34 +0800

    input

--- lines.js
+++ lines.js
@@ -1,6 +1,7 @@
 lines=[
-'<div style="text-align:center;font-size:3em;font-weight:nold;">測　試　展　示　程　式</div><var n="loop" v="-1" />',
-'<div>迴圈測試</div><p>LOOP: <var n="loop" v="+1" s /></p><p><if n="loop" c="ME" v="5">繞5次囉～<if n="loop" c="ME" v="10"> 別再繞啦～</if></if></p>',
-'<p><a gt=1>[進入迴圈]</a></p><p> </p><p><a gt>[離開迴圈]</a></p>',
-'尚未新增其他測試。'
+'<div style="text-align:center;font-size:3em;font-weight:nold;">測　試　展　示　程　式</div>',
+'<var n="loop" v="-1" /><div>該怎麼稱呼您呢？ <input n="name" maxL=12 v="Guest"><div style="float:right;"><a gt>[確認]</a></div><div style="clear:both"></div></div>',
+'<div>迴圈測試</div><p><var n="name" s />，您好。</p><p>LOOP: <var n="loop" v="+1" s /></p><p><if n="loop" c="ME" v="5">繞5次囉～<if n="loop" c="ME" v="10"> 別再繞啦～</if></if></p>',
+'<div><var n="name" s />，您要？</div><p><a gt=2>[進入迴圈]</a></p><p> </p><p><a gt>[離開迴圈]</a></p>',
+'<div>尚未新增其他測試。</div><p><var n="name" s /> 掰掰～</p>'
 ];
