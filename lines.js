ac22db3c7caba8dd50a2c0151eea6dcee44d686e
xNedKx <xnedkx@gmail.com>
2014-07-18 22:21:17 +0800

    clean up

--- lines.js
+++ lines.js
@@ -1,11 +1,11 @@
-//Writen By Lin, Arranged By Yz
-//
-/*tit=[
-'<div style="text-align:center;font-size:3em;font-weight:nold;">測　試　展　示　程　式</div>',//title
-'[開始]',//start button
-''//preload image page
-];*/
-//
+
+
+tit=[
+'<div style="text-align:center;font-size:3em;font-weight:nold;">測　試　展　示　程　式</div>',
+'[開始]',
+''
+];
+
 lines=[
 '<audio="http://sampleswap.org/mp3/artist/brettml/Seed-AI_2D-320.mp3" bg l /><div style="text-align:center;font-size:3em;font-weight:nold;">測　試　展　示　程　式</div><var n="loop" v="-1" />',
 '<skip /><var n="n" v=rnd /><var n="n" v="*5" /><var n="na" v="王大同" /><if n="n" c="ME" v="1"><var n="na" v="林小明" /><if n="n" c="ME" v="2"><var n="na" v="吳鳴世" /><if n="n" c="ME" v="3"><var n="na" v="秋森萬" /><if n="n" c="ME" v="4"><var n="na" v="艾隆麵" /></if></if></if></if><div>該怎麼稱呼您呢？ <input n="name" maxL=12 v=na /><if n="na" c="E" v=name gt=1 /><div style="float:right;"><a gt=1>[換個名子看看]</a><a gt>[確認]</a></div><div style="clear:both"></div></div>',
